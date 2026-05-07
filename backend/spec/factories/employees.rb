798b9b797d2a1110039c516c51e9a00cabf8ce3a
Kalpana Sharma <kalpana.sharma0795@gmail.com>
2026-05-07 13:05:38 +0530

    test: employee request spec for index action

--- backend/spec/factories/employees.rb
+++ backend/spec/factories/employees.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :employee do
-    full_name  { Faker::Name.full_name }
+    full_name  { Faker::Name.name }
     job_title  { Faker::Job.title }
     country    { Faker::Address.country_code }
     salary     { Faker::Number.between(from: 30_000, to: 250_000) }
---
+++ backend/spec/requests/employees_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'Employees API', type: :request do
+  describe 'GET /employees' do
+    before do
+      create_list(:employee, 3, country: 'India')
+    end
+
+    it 'returns all employees' do
+      get '/employees'
+
+      expect(response).to have_http_status(:ok)
+
+      body = JSON.parse(response.body)
+
+      expect(body['data'].length).to eq(3)
+      expect(body['meta']).to be_present
+    end
+
+    it 'supports pagination' do
+      create_list(:employee, 20)
+
+      get '/employees', params: { page: 1, per_page: 5 }
+
+      body = JSON.parse(response.body)
+
+      expect(body['data'].length).to eq(5)
+    end
+  end
+end
